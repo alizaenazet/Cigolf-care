325f56b4872cfcd8bdfee1bb417fb4ce8912fb06
alizaenazet <alizaenazet@icloud.com>
2025-08-28 11:18:41 +0700

    Initial Commit

---
+++ Palimanan-maintenance-team/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Palimanan-maintenance-team
+//
+//  Created by Ali zaenal on 28/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Palimanan-maintenance-team/Palimanan_maintenance_teamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Palimanan_maintenance_teamApp.swift
+//  Palimanan-maintenance-team
+//
+//  Created by Ali zaenal on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Palimanan_maintenance_teamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
