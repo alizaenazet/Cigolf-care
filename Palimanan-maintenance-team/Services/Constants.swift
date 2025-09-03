bfb07aa7eb9b2dceb4f8c8d5a5b89c21a97b5b15
yebology <139052533+yebology@users.noreply.github.com>
2025-09-03 19:02:05 +0700

    change loginUrl

--- Palimanan-maintenance-team/Services/Constants.swift
+++ Palimanan-maintenance-team/Services/Constants.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 struct Constants {
-    static let loginURL = "http://localhost:3000/api/v1/login"
+    static let loginURL = "https://cigolf-backend-yebology3212-s53p6k3p.apn.leapcell.dev/api/v1/login"
 }
